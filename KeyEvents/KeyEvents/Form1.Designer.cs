ea9fd294782974f7493c23e431c0509d1bb11bfb
Andre Beging <a.beging@eas-solutions.de>
2019-02-20 15:48:12 +0100

    Added KeyEvents Demo Project

---
+++ KeyEvents/KeyEvents/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace KeyEvents
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SpaceShipPanel = new System.Windows.Forms.Panel();
+            this.ControlInfoLabel = new System.Windows.Forms.Label();
+            this.SpaceShipTimer = new System.Windows.Forms.Timer(this.components);
+            this.PositionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SpaceShipPanel
+            // 
+            this.SpaceShipPanel.BackColor = System.Drawing.Color.LimeGreen;
+            this.SpaceShipPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SpaceShipPanel.Location = new System.Drawing.Point(366, 176);
+            this.SpaceShipPanel.Name = "SpaceShipPanel";
+            this.SpaceShipPanel.Size = new System.Drawing.Size(50, 50);
+            this.SpaceShipPanel.TabIndex = 0;
+            // 
+            // ControlInfoLabel
+            // 
+            this.ControlInfoLabel.AutoSize = true;
+            this.ControlInfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ControlInfoLabel.Location = new System.Drawing.Point(12, 9);
+            this.ControlInfoLabel.Name = "ControlInfoLabel";
+            this.ControlInfoLabel.Size = new System.Drawing.Size(122, 13);
+            this.ControlInfoLabel.TabIndex = 1;
+            this.ControlInfoLabel.Text = "Use your arrow keys";
+            // 
+            // SpaceShipTimer
+            // 
+            this.SpaceShipTimer.Interval = 10;
+            this.SpaceShipTimer.Tick += new System.EventHandler(this.SpaceShipTimer_Tick);
+            // 
+            // PositionLabel
+            // 
+            this.PositionLabel.AutoSize = true;
+            this.PositionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PositionLabel.Location = new System.Drawing.Point(12, 425);
+            this.PositionLabel.Name = "PositionLabel";
+            this.PositionLabel.Size = new System.Drawing.Size(45, 16);
+            this.PositionLabel.TabIndex = 2;
+            this.PositionLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PositionLabel);
+            this.Controls.Add(this.ControlInfoLabel);
+            this.Controls.Add(this.SpaceShipPanel);
+            this.Name = "Form1";
+            this.Text = "KeyEvents";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel SpaceShipPanel;
+        private System.Windows.Forms.Label ControlInfoLabel;
+        private System.Windows.Forms.Timer SpaceShipTimer;
+        private System.Windows.Forms.Label PositionLabel;
+    }
+}
+
---
+++ KeyEvents/KeyEvents/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace KeyEvents
+{
+    public partial class Form1 : Form
+    {
+        private bool _upKeyActive;
+        private bool _rightKeyActive;
+        private bool _downKeyActive;
+        private bool _leftKeyActive;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SpaceShipTimer.Enabled = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) _upKeyActive = true;
+            if (e.KeyCode == Keys.Down) _downKeyActive = true;
+            if (e.KeyCode == Keys.Left) _leftKeyActive = true;
+            if (e.KeyCode == Keys.Right) _rightKeyActive = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) _upKeyActive = false;
+            if (e.KeyCode == Keys.Down) _downKeyActive = false;
+            if (e.KeyCode == Keys.Left) _leftKeyActive = false;
+            if (e.KeyCode == Keys.Right) _rightKeyActive = false;
+        }
+
+        private void SpaceShipTimer_Tick(object sender, System.EventArgs e)
+        {
+            var distance = 10;
+            var maxX = Width - SpaceShipPanel.Width - distance;
+            var maxY = Height - SpaceShipPanel.Height - distance;
+
+            if (_upKeyActive)
+                SpaceShipPanel.Location = new Point(SpaceShipPanel.Location.X, SpaceShipPanel.Location.Y - distance);
+
+            if (_downKeyActive)
+                SpaceShipPanel.Location = new Point(SpaceShipPanel.Location.X, SpaceShipPanel.Location.Y + distance);
+
+            if (_leftKeyActive)
+                SpaceShipPanel.Location = new Point(SpaceShipPanel.Location.X - distance, SpaceShipPanel.Location.Y);
+
+            if (_rightKeyActive)
+                SpaceShipPanel.Location = new Point(SpaceShipPanel.Location.X + distance, SpaceShipPanel.Location.Y);
+
+            if (SpaceShipPanel.Location.X <= 0)
+                SpaceShipPanel.Location = new Point(0, SpaceShipPanel.Location.Y);
+
+            if (SpaceShipPanel.Location.X >= maxX)
+                SpaceShipPanel.Location = new Point(maxX, SpaceShipPanel.Location.Y);
+
+            if (SpaceShipPanel.Location.Y <= 0)
+                SpaceShipPanel.Location = new Point(SpaceShipPanel.Location.X, 0);
+
+            if (SpaceShipPanel.Location.Y >= maxY)
+                SpaceShipPanel.Location = new Point(SpaceShipPanel.Location.X, maxY);
+
+            PositionLabel.Text = "Position = X: " + SpaceShipPanel.Location.X + ", Y: " + SpaceShipPanel.Location.Y;
+        }
+    }
+}
